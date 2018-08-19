827f733eaaad863cd98f943336c2c99f47c31409
coffeeandscripts <erin.saricilar@gmail.com>
2018-08-19 12:56:25 +1000

    Populate board

--- chess/board.py
+++ chess/board.py
@@ -7,9 +7,13 @@ desc: classes to define the arrangement of the board
 vers: 0.0.1
 """
 
+from .piece import *
+import sys
+
 class Board:
     def __init__(self):
-        self.board = [[0]*8]*8;
+        self.board = [[" "]*8 for _ in range(8)]
+        self.populate_board()
 
     def __str__(self):
         return self.print_top_line() + self.print_board() + self.print_bottom_line()
@@ -23,7 +27,7 @@ class Board:
         for n in self.board:
             txt += " " + str(c) + " |"
             for m in n:
-                txt += " * |"
+                txt += " " + str(m) + " |"
             txt += " " + str(c) + "\n"
             if c > 1:
                 txt += "   |-------------------------------|\n"
@@ -33,3 +37,25 @@ class Board:
 
     def print_bottom_line(self):
         return ("   +-------------------------------+\n     A   B   C   D   E   F   G   H")
+
+    def populate_board(self):
+        self.board[0][0] = Castle(False)
+        self.board[0][1] = Knight(False)
+        self.board[0][2] = Bishop(False)
+        self.board[0][3] = Queen(False)
+        self.board[0][4] = King(False)
+        self.board[0][5] = Bishop(False)
+        self.board[0][6] = Knight(False)
+        self.board[0][7] = Castle(False)
+        for n in range(0,8):
+            self.board[1][n] = Pawn(False)
+        for n in range(0,8):
+            self.board[6][n] = Pawn(True)
+        self.board[7][0] = Castle(True)
+        self.board[7][1] = Knight(True)
+        self.board[7][2] = Bishop(True)
+        self.board[7][3] = King(True)
+        self.board[7][4] = Queen(True)
+        self.board[7][5] = Bishop(True)
+        self.board[7][6] = Knight(True)
+        self.board[7][7] = Castle(True)
--- chess/piece.py
+++ chess/piece.py
@@ -6,3 +6,69 @@ board.py
 desc: classes to define each piece on the board
 vers: 0.0.1
 """
+
+class Pawn:
+    def __init__(self, res):
+        self.user = res
+        self.char = "P"
+
+    def __str__(self):
+        if self.user:
+            return self.char
+        else:
+            return self.char.lower()
+        
+class Castle:
+    def __init__(self, res):
+        self.user = res
+        self.char = "H"
+
+    def __str__(self):
+        if self.user:
+            return self.char
+        else:
+            return self.char.lower()
+
+class Knight:
+    def __init__(self, res):
+        self.user = res
+        self.char = "Y"
+
+    def __str__(self):
+        if self.user:
+            return self.char
+        else:
+            return self.char.lower()
+
+class Bishop:
+    def __init__(self, res):
+        self.user = res
+        self.char = "B"
+
+    def __str__(self):
+        if self.user:
+            return self.char
+        else:
+            return self.char.lower()
+
+class Queen:
+    def __init__(self, res):
+        self.user = res
+        self.char = "Q"
+
+    def __str__(self):
+        if self.user:
+            return self.char
+        else:
+            return self.char.lower()
+
+class King:
+    def __init__(self, res):
+        self.user = res
+        self.char = "K"
+
+    def __str__(self):
+        if self.user:
+            return self.char
+        else:
+            return self.char.lower()
