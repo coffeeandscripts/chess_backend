ab07b2a6a825c5fa7aac4ca64e620d497a7e60b5
coffeeandscripts <erin.saricilar@gmail.com>
2018-08-19 11:47:01 +1000

    Printing board

--- chess/board.py
+++ chess/board.py
@@ -6,3 +6,30 @@ board.py
 desc: classes to define the arrangement of the board
 vers: 0.0.1
 """
+
+class Board:
+    def __init__(self):
+        self.board = [[0]*8]*8;
+
+    def __str__(self):
+        return self.print_top_line() + self.print_board() + self.print_bottom_line()
+
+    def print_top_line(self):
+        return ("     A   B   C   D   E   F   G   H\n   +-------------------------------+\n")
+
+    def print_board(self):
+        txt = ""
+        c = 8
+        for n in self.board:
+            txt += " " + str(c) + " |"
+            for m in n:
+                txt += " * |"
+            txt += " " + str(c) + "\n"
+            if c > 1:
+                txt += "   |-------------------------------|\n"
+            c -= 1
+        return txt
+                
+
+    def print_bottom_line(self):
+        return ("   +-------------------------------+\n     A   B   C   D   E   F   G   H")
--- chess/game.py
+++ chess/game.py
@@ -8,11 +8,13 @@ vers: 0.0.1
 """
 
 from datetime import datetime
+from .board import Board
 
 class Game:
     def __init__(self):
         self.start_time = datetime.now()
         self.last_updated = datetime.now()
+        self.board = Board()
 
     def __str__(self):
         return ("Chess: game started at " + str(self.start_time))
